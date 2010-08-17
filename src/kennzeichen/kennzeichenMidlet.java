71c433529791f63caa4866801c4330cda98ef8f1
Jan Hendriks <dahoc3150@googlemail.com>
2010-08-17 20:56:55 +0200

    initial commit

---
+++ src/kennzeichen/kennzeichenMidlet.java
@@ -0,0 +1,126 @@
+/**
+ * @author gawe design
+ */
+package kennzeichen;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.TextField;
+import javax.microedition.midlet.MIDlet;
+import kennzeichen.kennzeichendb;
+import kennzeichen.simpleImageCanvas;
+
+public class kennzeichenMidlet extends MIDlet implements CommandListener {
+
+    private Display display;
+    private Form form;
+    private simpleImageCanvas canvas;
+    private static final String SPLASH_IMAGE = "/images/gawesplash.png";
+    private static final String KENNZEICHEN_DB = "/database/kfzliste.csv";
+    private static kennzeichendb kdb;
+    private final Command exitCommand = new Command("Exit", Command.EXIT, 1);
+    private final Command search = new Command("Suche", Command.OK, 2);
+    private TextField badge = new TextField("Kennzeichen", "", 3, TextField.ANY);
+
+    public kennzeichenMidlet() {
+        this.display = Display.getDisplay(this);
+    }
+
+    public void startApp() {
+        this.showSplash();
+        form = new Form("Lade Datenbank");
+        display.setCurrent(form);
+        this.loadDatabase();
+        this.refreshForm();
+        form.append("");
+    }
+
+    private void refreshForm() {
+        form.setTitle("Kennzeichen suchen");
+        form.addCommand(search);
+        form.addCommand(exitCommand);
+        form.append(badge);
+        form.setCommandListener(this);
+    }
+
+    private void loadDatabase() {
+        // Reading resources of JAR file
+        InputStream inStream = this.getClass().getResourceAsStream(KENNZEICHEN_DB);
+        if (inStream == null) {
+            throw new Error("Failed to load database '" + KENNZEICHEN_DB + "': Not found!");
+        }
+        byte[] buf = new byte[14*1000]; // csv file is 13979 Bytes
+        if (inStream != null) {
+            try {
+                int total = 0;
+                while (true) {
+                    int numRead = inStream.read(buf, total, buf.length - total);
+                    if (numRead <= 0) {
+                        break;
+                    }
+                    total += numRead;
+                }
+                byte[] bufferWithCorrectLength = new byte[total];
+                System.arraycopy(buf, 0, bufferWithCorrectLength, 0, total);
+                kdb = new kennzeichendb(new String(bufferWithCorrectLength));
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    inStream.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void showSplash() {
+        canvas = new simpleImageCanvas(SPLASH_IMAGE);
+        display = Display.getDisplay(this);
+        canvas.setFullScreenMode(true);
+        display.setCurrent(canvas);
+        waitMilliseconds(2500); // Wait some time
+    }
+
+    private static void waitMilliseconds(long ms) {
+        try {
+            Thread.sleep(ms); // ms, e.g for pause to avoid cpu starvation
+        } catch (Exception ex) {
+            System.err.print("Thread backgrounding failed!");
+        }
+    }
+
+    public void commandAction(Command c, Displayable d) {
+        if (c == exitCommand) {
+            destroyApp(false);
+            System.out.println("Exit");
+        } else if (c == search) {
+            form.delete(form.size()-1);
+            String place = kdb.getPlaceOfBadge(this.badge.getString());
+            if (place.equalsIgnoreCase("")) {
+                form.append("Kennzeichen nicht gefunden!");
+            } else {
+                String state = kdb.getStateOfBadge(this.badge.getString());
+                if (!state.equalsIgnoreCase("")) {
+                    form.append(place + " (" + kdb.getStateOfBadge(this.badge.getString()) + ")");
+                } else {
+                    form.append(place);
+                }
+            }
+        }
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) {
+        System.out.println("Stopped");
+        notifyDestroyed();
+    }
+}
---
+++ src/kennzeichen/kennzeichendb.java
@@ -0,0 +1,66 @@
+/**
+ * @author gawe design
+ */
+package kennzeichen;
+
+import java.util.Hashtable;
+
+public class kennzeichendb {
+
+    // Separator strings for CSV
+    private static final String lineSeparator = "\n";
+    private static final String itemSeparator = ";";
+    private Hashtable places = new Hashtable();
+    private Hashtable states = new Hashtable();
+
+    // Constructor, takes the string read from the file and constructs an array out of it
+    public kennzeichendb(String kennzeichenString) {
+        // First split per line (=per row)
+        int index = 0;
+        int lastIndex = 0;
+        while ((index = kennzeichenString.indexOf(lineSeparator, lastIndex)) >= 0) {
+            this.extractItemsFromLine(kennzeichenString.substring(lastIndex, index));
+            lastIndex = index + lineSeparator.length();
+        }
+        // Add the last line
+        this.extractItemsFromLine(kennzeichenString.substring(lastIndex, kennzeichenString.length()));
+    }
+
+    // The CSV String has the form:
+    // A   ;Augsburg;(Bay)
+    private void extractItemsFromLine(String line) {
+        int firstIndex = line.indexOf(itemSeparator);
+        String index = line.substring(0, firstIndex).trim().toLowerCase();
+//        System.out.println("Key: " + index);
+        String place;
+        String state = "";
+        int secondIndex = line.indexOf(itemSeparator, firstIndex + 1);
+        if (secondIndex > 0) {
+            place = line.substring(firstIndex + 1, secondIndex).trim();
+//            System.out.println("Place: " + place);
+            int thirdIndex = line.indexOf(itemSeparator, secondIndex + 1);
+            if (thirdIndex > 0)
+                state = line.substring(secondIndex + 1, thirdIndex).trim();
+            else
+                state = line.substring(secondIndex + 1, line.length()).trim();
+//            System.out.println("State: " + state);
+        } else { // Special badges or no state given
+            place = line.substring(firstIndex + 1, line.length()).trim();
+//            System.out.println("Place: " + place);
+        }
+        places.put(index, place);
+        states.put(index, state);
+    }
+
+    // Retrieves the place from the given the badge
+    public String getPlaceOfBadge(String badge) {
+        badge = badge.trim().toLowerCase();
+        return (this.places.containsKey(badge) ? this.places.get(badge).toString() : "");
+    }
+
+    // Retrieves the state from the given the badge
+    public String getStateOfBadge(String badge) {
+        badge = badge.trim().toLowerCase();
+        return (this.states.containsKey(badge) ? this.states.get(badge).toString() : "");
+    }
+}
